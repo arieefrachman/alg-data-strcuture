81965a86ed7bb647693e0f4cbe74c1d8fe6007df
Arif Rakhman <kamucantiksekali@gmail.com>
2020-03-25 17:27:40 +0700

    complete find outlier

--- code-wars/test_find_outlier.py
+++ code-wars/test_find_outlier.py
@@ -1,7 +1,24 @@
 
 def find_outlier(arr):
-    print(arr)
+    odd = []
+    even = []
+    for i in range(len(arr)):
+        if arr[i] % 2 != 0:
+            odd.append(arr[i])
+        else:
+            even.append(arr[i])
+
+    if len(odd) == 1:
+        return odd[0]
+    else:
+        return even[0]
 
 
-a = [2, 4, 6, 8, 10, 3]
-find_outlier(a)
+def find_outlierI(int):
+    odds = [x for x in int if x%2!=0]
+    evens= [x for x in int if x%2==0]
+    return odds[0] if len(odds)<len(evens) else evens[0]
+
+
+def test_answer():
+    assert find_outlier([160, 3, 1719, 19, 11, 13, -21]) == 160
